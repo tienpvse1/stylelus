7770a96c0d952388a215438ae4adaffc038c1f97
tienpvse1 <tienpvse@fpt.edu.vn>
2022-01-02 14:19:47 +0700

    resolved(validation): add schema validation

--- src/modules/account/dto/create-account.dto.ts
+++ src/modules/account/dto/create-account.dto.ts
@@ -1,9 +1,14 @@
 import { ApiProperty } from '@nestjs/swagger';
+import { IsEmail, length, Length } from 'class-validator';
 
 export class CreateAccountDto {
+  @Length(2, 100)
   firstName: string;
+  @Length(2, 100)
   lastName: string;
+  @IsEmail()
   email: string;
+  @Length(6, 50)
   password: string;
   @ApiProperty({
     description: 'image url',
